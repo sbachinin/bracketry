35f7816873ef69a1e902ed76632bc64f3b569761
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-26 20:32:45 +0400

    Refactor (first create data for each match, then draw all)

--- lib.mjs
+++ lib.mjs
@@ -1,32 +1,41 @@
-import { getPlayerForMatch } from './utils.mjs'
+import { getPlayerMeta } from './utils.mjs'
+
+const ROUND_WIDTH = 150
+const ROUNDS_TITLE_HEIGHT = 100
+const MATCH_HEIGHT = 50
 
 export const drawBrackets = (data, canvasEl) => {
     if (canvasEl.getContext) {
         var ctx = canvasEl.getContext('2d');
-        
+
         // draw rounds
         ctx.font = '12px sans-serif';
         data.rounds.forEach((round, index) => {
             ctx.fillText(round.name, 150 * index, 50);
         })
 
-        // draw matches
-        const roundsToMatches = data.rounds.map(() => [])
-        
-        data.matches.forEach(match => {
-            const roundIndex = data.rounds.findIndex(r => r.uuid.match(match.round_id))
-            roundsToMatches[roundIndex].push(match)
-        })
+        const drawMatch = (matchData) => {
+            ctx.fillText(matchData.firstPlayer.name, matchData.positionX, matchData.positionY)
+            ctx.fillText(matchData.secondPlayer.name, matchData.positionX, matchData.positionY + 20)
+        }
 
-        roundsToMatches.forEach((matchesOfSingleRound, roundIndex) => {
-            matchesOfSingleRound
-            .sort((a, b) => a.order - b.order)
-            .forEach((match, matchIndex) => {
-                const firstPlayer = getPlayerForMatch(match, 0, data)
-                const secondPlayer = getPlayerForMatch(match, 1, data)
-                ctx.fillText(firstPlayer.short_name, 150 * roundIndex, 100 + 50 * matchIndex)
-                ctx.fillText(secondPlayer.short_name, 150 * roundIndex, 120 + 50 * matchIndex)
+        data.matches
+            .map(match => {
+                const roundIndex = data.rounds.findIndex(r => r.uuid.match(match.round_id))
+
+                return {
+                    firstPlayer: {
+                        name: getPlayerMeta(match, 0, data).short_name,
+                        score: match.teams[0].score.map(score => score.game)
+                    },
+                    secondPlayer: {
+                        name: getPlayerMeta(match, 1, data).short_name,
+                        score: match.teams[1].score.map(score => score.game)
+                    },
+                    positionX: ROUND_WIDTH * roundIndex,
+                    positionY: ROUNDS_TITLE_HEIGHT + MATCH_HEIGHT * match.order
+                }
             })
-        })
+            .forEach(drawMatch)
     }
 }
--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,4 @@
-export const getPlayerForMatch = (match, playerIndex, data) => {
+export const getPlayerMeta = (match, playerIndex, data) => {
     const playerId = data.teams.find(t => t.uuid.match(match.teams[playerIndex].team_id))?.players[0]
     return data.players.find(p => p.uuid === playerId)
 }
