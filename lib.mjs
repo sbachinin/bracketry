13bc30b3855bbc6aab5980c5d1589baf7abca825
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-26 18:58:46 +0400

    draw rounds and matches

--- lib.mjs
+++ lib.mjs
@@ -1,10 +1,30 @@
+import { getPlayerForMatch } from './utils.mjs'
+
 export const drawBrackets = (data, canvasEl) => {
-    console.log(data)
     if (canvasEl.getContext) {
         var ctx = canvasEl.getContext('2d');
-        // data.forEach((item, index) => {
-        //     ctx.fillStyle = 'rgb(200, 0, 0)';
-        //     ctx.fillRect(10, canvasEl.height / data.length * index, 5, 5);
-        // })
+        
+        // draw rounds
+        ctx.font = '10px sans-serif';
+        data.rounds.forEach((round, index) => {
+            ctx.fillText(round.name, 150 * index, 50);
+        })
+
+        // draw matches
+        const matchesForRound = data.rounds.map(() => [])
+        
+        data.matches.forEach(match => {
+            const roundIndex = data.rounds.findIndex(r => r.uuid.match(match.round_id))
+            matchesForRound[roundIndex].push(match)
+        })
+
+        matchesForRound.forEach((matches, roundIndex) => {
+            matches.forEach((match, matchIndex) => {
+                const firstPlayer = getPlayerForMatch(match, 0, data)
+                const secondPlayer = getPlayerForMatch(match, 1, data)
+                const rivals = firstPlayer.short_name + ' : ' + secondPlayer.short_name
+                ctx.fillText(rivals, 150 * roundIndex, 100 + 50 * matchIndex)
+            })
+        })
     }
 }
