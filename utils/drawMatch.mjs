ac45e7e29d0de6035ffa8f48637c9395e1a15f72
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-27 15:44:19 +0400

    try (& comment out) to draw a line to parent match

--- utils/drawMatch.mjs
+++ utils/drawMatch.mjs
@@ -1,20 +1,33 @@
 import * as sizes from './sizes.mjs'
 
-export const drawMatch = (matchData, canvas) => {
+// const drawLineToFirstParentPair = (matchData, ctx) => {
+//     const parentY = matchData.positionY + sizes.MATCH_HEIGHT / 2
+//     ctx.beginPath();
+//     ctx.moveTo(
+//         matchData.positionX + sizes.MATCH_HOR_MARGIN,
+//         matchData.positionY + sizes.MATCH_HEIGHT / 2);
+//     ctx.lineTo(matchData.positionX - sizes.MATCH_HOR_MARGIN,
+//         parentY);
+//     ctx.stroke();
+// }
 
-    canvas.rect(
+export const drawMatch = (matchData, ctx) => {
+
+    ctx.rect(
         matchData.positionX + sizes.MATCH_HOR_MARGIN,
         matchData.positionY,
         sizes.ROUND_WIDTH - sizes.MATCH_HOR_MARGIN * 2,
         sizes.MATCH_HEIGHT)
-    canvas.stroke();
+    ctx.stroke();
 
-    canvas.fillText(
+    ctx.fillText(
         matchData.firstPlayer.name,
         matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
         matchData.positionY + sizes.FONT_SIZE + sizes.MATCH_VERT_PADDING)
-    canvas.fillText(
+    ctx.fillText(
         matchData.secondPlayer.name,
         matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
         matchData.positionY + sizes.MATCH_HEIGHT - sizes.MATCH_VERT_PADDING - 2)
+
+    // drawLineToFirstParentPair(matchData, ctx)
 }
