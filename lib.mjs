62713c74b5d9bb0497fe3ccf131b1e89af045df9
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-27 11:06:10 +0400

    adjust sizes

--- lib.mjs
+++ lib.mjs
@@ -1,38 +1,49 @@
 import { getPlayerMeta } from './utils.mjs'
-
-const ROUND_WIDTH = 150
-const ROUNDS_TITLE_HEIGHT = 100
-const MATCH_HEIGHT = 50
+import * as sizes from './sizes.mjs'
 
 export const drawBrackets = (data, canvasEl) => {
     if (canvasEl.getContext) {
         var ctx = canvasEl.getContext('2d');
 
         // draw rounds
-        ctx.font = '12px sans-serif';
+        ctx.font = sizes.FONT_SIZE + 'px sans-serif';
         data.rounds.forEach((round, index) => {
             ctx.fillText(round.name, 150 * index, 50);
         })
 
         const drawMatch = (matchData) => {
-            ctx.fillText(matchData.firstPlayer.name, matchData.positionX, matchData.positionY)
-            ctx.fillText(matchData.secondPlayer.name, matchData.positionX, matchData.positionY + 20)
+
+            ctx.rect(
+                matchData.positionX + sizes.MATCH_HOR_MARGIN,
+                matchData.positionY,
+                sizes.ROUND_WIDTH - sizes.MATCH_HOR_MARGIN * 2,
+                sizes.MATCH_HEIGHT)
+            ctx.stroke();
+
+            ctx.fillText(
+                matchData.firstPlayer.name,
+                matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
+                matchData.positionY + sizes.FONT_SIZE + sizes.MATCH_VERT_PADDING)
+            ctx.fillText(
+                matchData.secondPlayer.name,
+                matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
+                matchData.positionY + sizes.MATCH_HEIGHT - sizes.MATCH_VERT_PADDING - 2)
         }
 
         const getRoundHighestY = (roundIndex) => {
             const distanceFromFinal = data.rounds.length - roundIndex - 1
             const numberOfMatches = Math.pow(2, distanceFromFinal)
-            const roundHeight = numberOfMatches * MATCH_HEIGHT
-            
+            const roundHeight = numberOfMatches * sizes.MATCH_HEIGHT
+
             // create an upper margin to center matches vertically in case they all fit the canvas
-            const roundVerticalMargin = Math.max(0, (canvasEl.height - roundHeight) / 2)
-            return ROUNDS_TITLE_HEIGHT + roundVerticalMargin
+            const roundVerticalMargin = Math.max(0, (canvasEl.height - sizes.ROUNDS_TITLE_HEIGHT - roundHeight) / 2)
+            return sizes.ROUNDS_TITLE_HEIGHT + roundVerticalMargin
         }
 
         data.matches
             .map(match => {
                 const roundIndex = data.rounds.findIndex(r => r.uuid.match(match.round_id))
-                
+
                 const roundHighestY = getRoundHighestY(roundIndex)
 
                 return {
@@ -44,8 +55,8 @@ export const drawBrackets = (data, canvasEl) => {
                         name: getPlayerMeta(match, 1, data).short_name,
                         score: match.teams[1].score.map(score => score.game)
                     },
-                    positionX: ROUND_WIDTH * roundIndex,
-                    positionY: roundHighestY + MATCH_HEIGHT * (match.order - 1)
+                    positionX: sizes.ROUND_WIDTH * roundIndex,
+                    positionY: roundHighestY + (sizes.MATCH_HEIGHT + sizes.MATCH_VERT_MARGIN) * (match.order - 1)
                 }
             })
             .forEach(drawMatch)
---
+++ sizes.mjs
@@ -0,0 +1,8 @@
+export const ROUND_WIDTH = 150
+export const MATCH_HOR_MARGIN = 10
+export const MATCH_VERT_MARGIN = 10
+export const MATCH_HOR_PADDING = 5
+export const MATCH_VERT_PADDING = 5
+export const ROUNDS_TITLE_HEIGHT = 100
+export const MATCH_HEIGHT = 45
+export const FONT_SIZE = 14
