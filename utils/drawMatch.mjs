449ff5c6685744946a1de44a7e4a22f9c6fc5953
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-27 20:55:17 +0400

    adjust sizes

--- utils/drawMatch.mjs
+++ utils/drawMatch.mjs
@@ -1,22 +1,25 @@
 import * as sizes from './sizes.mjs'
 
 export const drawMatch = (matchData, ctx) => {
-    const matchBodyHeight = Math.min(matchData.availableHeight, sizes.MATCH_MAX_HEIGHT)
+    const matchBodyWidth = sizes.ROUND_WIDTH - sizes.MATCH_HOR_MARGIN * 2
+    const matchBodyHeight = Math.min(matchData.availableHeight, sizes.MATCH_MAX_HEIGHT) * 0.8
     const centerY = matchData.positionY + matchData.availableHeight / 2
+    const matchBodyY = centerY - matchBodyHeight / 2
 
-    ctx.rect(
+    ctx.fillStyle = '#feefe3'
+    ctx.fillRect(
         matchData.positionX + sizes.MATCH_HOR_MARGIN,
-        centerY - matchBodyHeight / 2,
-        sizes.ROUND_WIDTH - sizes.MATCH_HOR_MARGIN * 2,
+        matchBodyY,
+        matchBodyWidth,
         matchBodyHeight)
-    ctx.stroke();
 
+    ctx.fillStyle = 'black'
     ctx.fillText(
         matchData.firstPlayer.name,
         matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
-        centerY)
+        centerY - matchBodyHeight / 10)
     ctx.fillText(
         matchData.secondPlayer.name,
         matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
-        centerY + sizes.FONT_SIZE)
+        centerY + sizes.FONT_SIZE + matchBodyHeight / 10)
 }
--- utils/sizes.mjs
+++ utils/sizes.mjs
@@ -1,10 +1,10 @@
 export const ROUNDS_TITLE_HEIGHT = 100
 
-export const ROUND_WIDTH = 150
-export const MATCH_HOR_MARGIN = 10
+export const ROUND_WIDTH = 200
+export const MATCH_HOR_MARGIN = 15
 export const MATCH_VERT_MARGIN = 10
 export const MATCH_HOR_PADDING = 5
 export const MATCH_VERT_PADDING = 5
-export const MATCH_MIN_HEIGHT = 45
+export const MATCH_MIN_HEIGHT = 55
 export const MATCH_MAX_HEIGHT = 70
 export const FONT_SIZE = 14
