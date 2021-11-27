19446ce273bcbdd134d8b8ea5fc4d4d38cefdbec
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-27 21:50:00 +0400

    adjust canvas size on window resize

--- lib.mjs
+++ lib.mjs
@@ -1,24 +1,39 @@
 import { createMatchDrawingData } from './utils/createMatchDrawingData.mjs'
+import { debounce } from './utils/utils.mjs'
 import { drawMatch } from './utils/drawMatch.mjs'
 import * as sizes from './utils/sizes.mjs'
 
+const draw = (data, canvasEl) => {
+    if (!canvasEl.getContext) return
+    const ctx = canvasEl.getContext('2d');
+
+    // draw rounds
+    ctx.font = sizes.FONT_SIZE + 'px sans-serif';
+    data.rounds.forEach((round, index) => {
+        ctx.fillText(round.name, 150 * index, 50);
+    })
+
+    data.matches
+        .map(match => createMatchDrawingData(match, data, canvasEl))
+        .forEach(match => drawMatch(match, ctx))
+}
+
 export const drawBrackets = (data, canvasContainer) => {
+    // create canvas to fill a given element
     const canvasEl = document.createElement('canvas');
-    canvasEl.setAttribute('width', canvasContainer.clientWidth + "px");
-    canvasEl.setAttribute('height', canvasContainer.clientHeight + "px");
+    canvasEl.width = canvasContainer.clientWidth;
+    canvasEl.height = canvasContainer.clientHeight;
     canvasContainer.appendChild(canvasEl);
 
-    if (canvasEl.getContext) {
-        var ctx = canvasEl.getContext('2d');
+    window.addEventListener(
+        'resize',
+        debounce(() => {
+            canvasEl.width = canvasContainer.clientWidth;
+            canvasEl.height = canvasContainer.clientHeight;
+            draw(data, canvasEl)
+        }),
+        false
+    );
 
-        // draw rounds
-        ctx.font = sizes.FONT_SIZE + 'px sans-serif';
-        data.rounds.forEach((round, index) => {
-            ctx.fillText(round.name, 150 * index, 50);
-        })
-
-        data.matches
-            .map(match => createMatchDrawingData(match, data, canvasEl))
-            .forEach(match => drawMatch(match, ctx))
-    }
+    draw(data, canvasEl)
 }
