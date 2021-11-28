ca17a3095e59ff1cda31983e6c1244ef48c22368
Sergey Bachinin <sbachinin@yandex.ru>
2021-11-28 22:32:44 +0400

    Make all drawing data visible when drawing specific match

--- lib.mjs
+++ lib.mjs
@@ -1,21 +1,71 @@
 import { createCanvas } from './utils/createCanvas.mjs'
-import { drawMatchesForRound } from './utils/drawMatchesForRound.mjs'
+import { getMatchDrawingData } from './utils/getMatchDrawingData.mjs'
 import * as sizes from './utils/sizes.mjs'
+
+const drawMatchesForRound = (roundIndex, allRounds, ctx) => {
+    const drawLineToFirstParentPair = (positionX, centerY, ctx) => {
+        const parentY = centerY
+        ctx.beginPath();
+        ctx.moveTo(
+            positionX + sizes.MATCH_HOR_MARGIN,
+            centerY);
+        ctx.lineTo(positionX - sizes.MATCH_HOR_MARGIN,
+            parentY);
+        ctx.stroke();
+    }
+    
+    allRounds[roundIndex].matchesToDraw.forEach(matchData => {
+    
+        ctx.fillStyle = '#feefe3'
+        ctx.fillRect(
+            matchData.positionX + sizes.MATCH_HOR_MARGIN,
+            matchData.matchBodyY,
+            matchData.matchBodyWidth,
+            matchData.matchBodyHeight)
+    
+        ctx.fillStyle = 'black'
+        ctx.fillText(
+            matchData.firstTeamTitle,
+            matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
+            matchData.centerY - matchData.matchBodyHeight / 10)
+        ctx.fillText(
+            matchData.secondTeamTitle,
+            matchData.positionX + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
+            matchData.centerY + sizes.FONT_SIZE + matchData.matchBodyHeight / 10)
+
+        drawLineToFirstParentPair(matchData.positionX, matchData.centerY, ctx)
+    })
+}
+
+const drawRounds = (allRounds, ctx) => {
+    allRounds.forEach((roundData, roundIndex) => {
+        ctx.font = sizes.HEADING_FONT_SIZE + 'px sans-serif';
+        ctx.fillText(
+            roundData.name,
+            sizes.ROUND_WIDTH * roundIndex + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
+            50);
+    
+        ctx.font = sizes.FONT_SIZE + 'px sans-serif';
+        drawMatchesForRound(roundIndex, allRounds, ctx)
+    })
+}
 
 const draw = (allData, canvasEl) => {
     if (!canvasEl.getContext) return
     const ctx = canvasEl.getContext('2d');
 
-    allData.rounds.forEach((round, roundIndex) => {
-        ctx.font = sizes.HEADING_FONT_SIZE + 'px sans-serif';
-        ctx.fillText(
-            round.name,
-            sizes.ROUND_WIDTH * roundIndex + sizes.MATCH_HOR_MARGIN + sizes.MATCH_HOR_PADDING,
-            50);
+    const roundsWithDrawingData = allData.rounds.map((roundData, roundIndex) => {
+        const visibleHeightPerMatch = (canvasEl.height - sizes.ROUNDS_TITLE_HEIGHT) / roundData.matches.length
+        const freeHeightPerMatch = Math.max(visibleHeightPerMatch, sizes.MATCH_MIN_HEIGHT)
 
-            ctx.font = sizes.FONT_SIZE + 'px sans-serif';
-        drawMatchesForRound(round, roundIndex, canvasEl)
+        return {
+            ...roundData,
+            matchesToDraw: roundData.matches.map(
+                match => getMatchDrawingData(match, freeHeightPerMatch, roundIndex))
+        }
     })
+
+    drawRounds(roundsWithDrawingData, ctx)
 }
 
 export const createBrackets = (allData, canvasContainer) => {
---
+++ utils/getMatchDrawingData.mjs
@@ -0,0 +1,26 @@
+import * as sizes from './sizes.mjs'
+
+export const getMatchDrawingData = (matchData, freeHeightPerMatch, roundIndex) => {
+    const score = matchData.teams[1].score.map(score => score.game)
+    const positionX = sizes.ROUND_WIDTH * roundIndex
+    const positionY = sizes.ROUNDS_TITLE_HEIGHT + freeHeightPerMatch * (matchData.order - 1)
+
+    const matchBodyWidth = sizes.ROUND_WIDTH - sizes.MATCH_HOR_MARGIN * 2
+    const matchBodyHeight = Math.min(freeHeightPerMatch, sizes.MATCH_MAX_HEIGHT) * 0.8
+    const centerY = positionY + freeHeightPerMatch / 2
+    const matchBodyY = centerY - matchBodyHeight / 2
+    const firstTeamTitle = matchData.teams[0].players[0].shortName
+    const secondTeamTitle = matchData.teams[1].players[0].shortName
+
+    return {
+        positionX,
+        positionY,
+        matchBodyWidth,
+        matchBodyHeight,
+        centerY,
+        matchBodyY,
+        score,
+        firstTeamTitle,
+        secondTeamTitle
+    }
+}
