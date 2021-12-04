d103cb5416d1004b4e5345914c355f84baa91ad8
Sergey Bachinin <sbachinin@yandex.ru>
2021-12-04 14:00:41 +0400

    Make buttons change the horizontall scroll pos

--- lib.mjs
+++ lib.mjs
@@ -2,16 +2,27 @@ import { createCanvas } from './utils/createCanvas.mjs'
 import { drawAll } from './utils/draw_all.mjs'
 import { create_horizontal_scroll_buttons } from './utils/create_horizontal_scroll_buttons.mjs'
 import { installMouseEvents } from './utils/install_mouse_events.mjs'
+import * as sizes from './utils/sizes.mjs'
 
 export const createBrackets = (allData, canvasContainer, options) => {
     const state = {
         scrollY: 0,
         scrollX: 0
     }
-    
+
+    const change_round_index = new_leftmost_round_index => {
+        state.scrollX = -new_leftmost_round_index * sizes.ROUND_WIDTH
+        drawAll(allData, state, canvasEl)
+    }
+
     options.horizontalScrollTriggeredBy === 'buttons'
-        && create_horizontal_scroll_buttons(canvasContainer, options)
-    
+        && create_horizontal_scroll_buttons(
+            canvasContainer,
+            options.backgroundColor,
+            allData.rounds.length,
+            state,
+            change_round_index)
+
     const canvasEl = createCanvas(
         canvasContainer,
         options,
--- utils/create_horizontal_scroll_buttons.mjs
+++ utils/create_horizontal_scroll_buttons.mjs
@@ -1,7 +1,7 @@
 import { create_unique_id } from './utils.mjs'
 import * as sizes from './sizes.mjs'
 
-const getButtonsStyle = (canvasContainer, id, options) => `
+const getButtonsStyle = (canvasContainer, id, bgColor) => `
     .${id}.buttons-container {
         opacity: 0;
         display: block;
@@ -16,7 +16,7 @@ const getButtonsStyle = (canvasContainer, id, options) => `
         top: -${canvasContainer.clientHeight}px;
         height: ${sizes.ROUNDS_TITLE_HEIGHT}px;
         width: 100%;
-        background: linear-gradient(to right, ${options.backgroundColor} 2%, rgba(0,0,0,0%) 10%, rgba(0,0,0,0%) 90%, ${options.backgroundColor} 98%);
+        background: linear-gradient(to right, ${bgColor} 2%, rgba(0,0,0,0%) 10%, rgba(0,0,0,0%) 90%, ${bgColor} 98%);
     }
 
     canvas.sport-brackets-canvas:hover ~ .${id}, .${id}:hover {
@@ -42,6 +42,12 @@ const getButtonsStyle = (canvasContainer, id, options) => `
         padding-top: 2px;
         background-color: #2d2d2d;
         color: white;
+        user-select: none;
+    }
+
+    .${id} .scroll-rounds-button.hidden {
+        visibility: hidden;
+        pointer-events: none;
     }
     
     .${id} .scroll-rounds-button:hover {
@@ -59,19 +65,79 @@ const getButtonsStyle = (canvasContainer, id, options) => `
     }
 `
 
-export const create_horizontal_scroll_buttons = (canvasContainer, options) => {
-    const id = create_unique_id()
+const get_leftmost_index = scrollX => Math.abs(Math.floor(scrollX / sizes.ROUND_WIDTH))
 
+const update_buttons_visibility = (
+    new_leftmost_round_index,
+    invisible_rounds_count,
+    leftButton, rightButton
+) => {
+    if (new_leftmost_round_index <= 0) {
+        leftButton.classList.add('hidden')
+    } else {
+        leftButton.classList.remove('hidden')
+    }
+    if (new_leftmost_round_index >= invisible_rounds_count) {
+        rightButton.classList.add('hidden')
+    } else {
+        rightButton.classList.remove('hidden')
+    }
+}
+
+const createButton = (state, side, handle_new_round_index) => {
+    const button = document.createElement('div')
+    button.className = [
+        'scroll-rounds-button',
+        `scroll-rounds-button-${side}`,
+    ].join(' ')
+    button.innerHTML = side === 'left' ? '&#60;' : '&#62;'
+    button.addEventListener('click', () => {        
+        handle_new_round_index(
+            get_leftmost_index(state.scrollX)
+            + (side === 'left' ? -1 : 1)
+        )
+    })
+    return button
+}
+
+
+
+
+export const create_horizontal_scroll_buttons = (
+    canvasContainer,
+    bgColor,
+    rounds_count,
+    state,
+    change_round_index
+) => {
+    const fully_visible_rounds_count = Math.floor(canvasContainer.clientWidth / sizes.ROUND_WIDTH)
+    const invisible_rounds_count = rounds_count - fully_visible_rounds_count
+
+    const id = create_unique_id()
     document.head.insertAdjacentHTML(
         'beforeend',
-        `<style>${getButtonsStyle(canvasContainer, id, options)}</style>`
+        `<style>${getButtonsStyle(canvasContainer, id, bgColor)}</style>`
     )
 
-    canvasContainer.innerHTML += `
-        <div class='${id} buttons-container'>
-            <div class='buttons-bg'></div>
-            <div class='scroll-rounds-button-left scroll-rounds-button'>&#60;</div>
-            <div class='scroll-rounds-button-right scroll-rounds-button'>&#62;</div>
-        </div>
-    `
+    const handle_new_round_index = new_index => {
+        update_buttons_visibility(
+            new_index,
+            invisible_rounds_count,
+            leftButton, rightButton
+        )
+        change_round_index(new_index)
+    }
+
+    const buttonsContainer = document.createElement('div')
+    buttonsContainer.className = `${id} buttons-container`
+    buttonsContainer.innerHTML = "<div class='buttons-bg'></div>"
+    const leftButton = createButton(state, 'left', handle_new_round_index)
+    const rightButton = createButton(state, 'right', handle_new_round_index)
+    update_buttons_visibility(
+        get_leftmost_index(state.scrollX),
+        invisible_rounds_count,
+        leftButton, rightButton
+    )
+    buttonsContainer.append(leftButton, rightButton)
+    canvasContainer.appendChild(buttonsContainer)
 }
