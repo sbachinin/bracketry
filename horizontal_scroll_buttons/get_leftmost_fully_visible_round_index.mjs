484d0e7e6ad08f9a2c536ff17d886663398c77b5
Sergey Bachinin <sbachinin@yandex.ru>
2021-12-06 21:55:55 +0400

    allow 10% mistake when getting leftmost round

--- horizontal_scroll_buttons/get_leftmost_fully_visible_round_index.mjs
+++ horizontal_scroll_buttons/get_leftmost_fully_visible_round_index.mjs
@@ -1,3 +1,10 @@
 import * as constants from '../utils/constants.mjs'
 
-export const get_leftmost_fully_visible_round_index = scrollX => Math.ceil(Math.abs(scrollX / constants.ROUND_WIDTH))
+export const get_leftmost_fully_visible_round_index = scrollX => {
+    const outscrolled_rounds_count = Math.abs(scrollX / constants.ROUND_WIDTH)
+    // if the leftmost round is hidden by less then 10%, consider it fully visible
+    if (outscrolled_rounds_count % 1 < 0.1) {
+        return Math.floor(Math.abs(scrollX / constants.ROUND_WIDTH))
+    }
+    return Math.ceil(Math.abs(scrollX / constants.ROUND_WIDTH))
+}
