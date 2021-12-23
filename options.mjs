7e36e5b9a5f8854ee08201c6d127bf514c514419
Sergey Bachinin <sbachinin@yandex.ru>
2021-12-23 20:59:30 +0400

    display options inputs

--- options.mjs
+++ options.mjs
@@ -1,35 +1,43 @@
 export const OPTIONS = {
     backgroundColor: {
+        title: 'Canvas backgound color',
         type: 'string',
         default_value: '#ffffff'
     },
     horizontal_scroll_triggered_by: {
+        title: 'Bracket scrolled horizontally by',
         type: 'select',
         options: [ 'buttons', 'mousemove' ],
         default_value: 'buttons'
     },
     horizontal_scroll_buttons_position: {
+        title: 'Horizontal scroll buttons positioned at',
         type: 'select',
         options: [ 'top', 'middle', 'bottom' ],
         default_value: 'top'
     },
     horizontal_scroll_buttons_size: {
+        title: 'Horizontal scroll buttons size',
         type: 'number',
         default_value: 32
     },
     horizontal_scroll_buttons_color: {
+        title: 'Horizontal scroll buttons color',
         type: 'string',
         default_value:'#161616'
     },
     horizontal_scroll_buttons_icon_left: {
+        title: 'Icon for left horizontal scroll button',
         type: 'string',
         default_value: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>'
     },
     horizontal_scroll_buttons_icon_right: {
+        title: 'Icon for right horizontal scroll button',
         type: 'string',
         default_value: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
     },
     connection_lines_type: {
+        title: 'Connection lines type',
         type: 'select',
         options: [ 'curve', 'diagonal', 'bended-1', 'bended-2' ],
         default_value: 'bended-2'
