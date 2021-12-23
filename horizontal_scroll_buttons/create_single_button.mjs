d2744af836a5248b15db361c535048fd61192a68
Sergey Bachinin <sbachinin@yandex.ru>
2021-12-23 10:54:40 +0400

    make prettier buttons

--- horizontal_scroll_buttons/create_single_button.mjs
+++ horizontal_scroll_buttons/create_single_button.mjs
@@ -1,5 +1,8 @@
 import { update_button_visibility } from './update_button_visibility.mjs'
 
+const arrowLeft = `<svg width="32" height="32" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>`
+const arrowRight = `<svg width="32" height="32" viewBox="0 0 24 24"><path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/></svg>`
+
 export const create_single_button = (
     side,
     onclick,
@@ -12,7 +15,7 @@ export const create_single_button = (
     ].join(' ')
 
     button.innerHTML = `<div class="button-icon">${
-        side === 'left' ? '&#60;' : '&#62;'
+        side === 'left' ? arrowLeft : arrowRight
     }</div>`
 
     button.addEventListener('click', onclick)
--- horizontal_scroll_buttons/insert_buttons_styles.mjs
+++ horizontal_scroll_buttons/insert_buttons_styles.mjs
@@ -1,9 +1,9 @@
 const get_top = position => {
     if (position === 'top') {
-        return '20px'
+        return '0'
     }
     if (position === 'bottom') {
-        return 'calc(100% - 70px)'
+        return 'calc(100% - 86px)'
     }
     return 'calc(50% - 25px)'
 }
@@ -13,11 +13,13 @@ export const get_buttons_style = (root_id, options) => `
         opacity: 0;
         display: flex;
         justify-content: center;
+        align-items: center;
         position: absolute;
-        height: 50px;
+        height: 86px;
         width: 86px;
         top: ${get_top(options.horizontal_scroll_buttons_position)};
         user-select: none;
+        background: radial-gradient(${options.backgroundColor}, rgba(0,0,0,0));
     }
 
     .${root_id}:hover .scroll-rounds-button {
@@ -31,15 +33,11 @@ export const get_buttons_style = (root_id, options) => `
         font-size: 50px;
         font-family: arial;
         cursor: pointer;
-        border: 2px solid black;
-        border-radius: 50px;
         justify-content: center;
         align-items: center;
         box-sizing: border-box;
         padding-top: 2px;
-        background-color: #2d2d2d;
         color: white;
-        opacity: 0.3;
     }
 
     .${root_id} .scroll-rounds-button.hidden {
@@ -53,9 +51,7 @@ export const get_buttons_style = (root_id, options) => `
     }
 
     .${root_id} .scroll-rounds-button-left {
-        padding-right: 40px;
         left: 0;
-        background: linear-gradient(to right, ${options.bgColor} 50%, rgba(0,0,0,0%) 100%);
     }
 
     .${root_id} .scroll-rounds-button-left .button-icon {
@@ -63,9 +59,7 @@ export const get_buttons_style = (root_id, options) => `
     }
 
     .${root_id} .scroll-rounds-button-right  {
-        padding-left: 40px;
         right: 0;
-        background: linear-gradient(to left, ${options.bgColor} 50%, rgba(0,0,0,0%) 100%);
     }
 
     .${root_id} .scroll-rounds-button-right .button-icon {
