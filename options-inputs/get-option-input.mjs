90d3c213c45a6860c66b3e788df680fea31517e9
Sergey Bachinin <sbachinin@yandex.ru>
2021-12-30 13:29:21 +0400

    display options grouped

--- options-inputs/get-option-input.mjs
+++ options-inputs/get-option-input.mjs
@@ -1,8 +1,8 @@
-import { createElementFromHTML, escapeHtml } from './utils.mjs'
+import { create_element_from_Html, escape_Html } from './utils.mjs'
 
 export const get_option_input = (name, info, value, onchange) => {
     if (info.type === 'number') {
-        const el = createElementFromHTML(`
+        const el = create_element_from_Html(`
             <input type='number' value='${value}'></input>
         `)
         el.addEventListener('input', e => {
@@ -11,8 +11,8 @@ export const get_option_input = (name, info, value, onchange) => {
         return el
     }
     if (info.type === 'string') {
-        const el = createElementFromHTML(`
-            <input type='text' value='${escapeHtml(value)}'></input>
+        const el = create_element_from_Html(`
+            <input type='text' value='${escape_Html(value)}'></input>
         `)
         el.addEventListener('input', e => {
             onchange(name, e.target.value)
@@ -20,7 +20,7 @@ export const get_option_input = (name, info, value, onchange) => {
         return el
     }
     if (info.type === 'multiline_string') {
-        const el = createElementFromHTML(`
+        const el = create_element_from_Html(`
             <textarea style="width: 100%; height: 100px;">${value}</textarea>
         `)
         el.addEventListener('input', e => {
@@ -29,7 +29,7 @@ export const get_option_input = (name, info, value, onchange) => {
         return el
     }
     if (info.type === 'select') {
-        const el = createElementFromHTML(`
+        const el = create_element_from_Html(`
             <select name="..." id="...">
                 ${info.options.map(opt => `
                     <option value=${opt} ${opt === value ? 'selected' : ''}>
@@ -44,7 +44,7 @@ export const get_option_input = (name, info, value, onchange) => {
         return el
     }
     if(info.type === 'switch') {
-        const el = createElementFromHTML(`
+        const el = create_element_from_Html(`
             <label class="switch">
                 <input type="checkbox" ${value ? 'checked' : ''}>
                 <span class="slider round"></span>
--- options-inputs/options-inputs.mjs
+++ options-inputs/options-inputs.mjs
@@ -1,7 +1,24 @@
-import { get_default_options, get_flattened_options } from '../lib/utils/get_default_options.mjs'
+import * as OPTIONS from '../lib/options.mjs'
+import { get_default_options } from '../lib/utils/get_default_options.mjs'
 import { switchStyle } from './switch-style.mjs'
-import { createElementFromHTML, escapeHtml } from './utils.mjs'
+import { create_element_from_Html, escape_Html } from './utils.mjs'
 import { get_option_input } from './get-option-input.mjs'
+
+const get_options_type_text_el = options_type_name => {
+    const text = options_type_name
+        .split('_')
+        .filter(word => word !== 'OPTIONS')
+        .map(word => word[0].toUpperCase() + word.slice(-word.length + 1).toLowerCase())
+        .join(' ')
+    
+    return create_element_from_Html(`
+        <div style='
+            font-size: 24px;
+            font-family: roboto;
+            padding: 12px 0 6px 10px;
+        '>${text}</div>
+    `)
+}
 
 export const get_options_inputs = (handle_options_change, user_options_to_values) => {
     const default_options_to_values = get_default_options()
@@ -14,32 +31,42 @@ export const get_options_inputs = (handle_options_change, user_options_to_values
         )
         handle_options_change(options_to_values)
     }
-    
-    const inputs = Object.entries(get_flattened_options())
-        .map(([option_name, option_info]) => {
-            const option_wrapper_el = createElementFromHTML(
-                `<div style='margin: 5px; background: tomato; padding: 5px;'>
-                    <p style='margin: 3px'>${escapeHtml(option_info.title)}</p>
-                </div>`
-            )
-            const input = get_option_input(
-                option_name,
-                option_info,
-                options_to_values[option_name],
-                onchange
-            )
-            option_wrapper_el.append(input)
-            return option_wrapper_el
-        })
 
-    const wrapper_el = createElementFromHTML(
+    const get_inputs_of_type = options => {
+        return Object.entries(options)
+            .map(([option_name, option_info]) => {
+                const option_wrapper_el = create_element_from_Html(
+                    `<div style='margin: 5px; background: tomato; padding: 5px;'>
+                        <p style='margin: 3px'>${escape_Html(option_info.title)}</p>
+                    </div>`
+                )
+                const input = get_option_input(
+                    option_name,
+                    option_info,
+                    options_to_values[option_name],
+                    onchange
+                )
+                option_wrapper_el.append(input)
+                return option_wrapper_el
+            })
+    }
+
+    const wrapper_el = create_element_from_Html(
         `<div style='
             width: 400px;
             height: 98vh;
             overflow: scroll;
             flex-shrink: 0;
         '></div>`)
-    wrapper_el.append(...inputs)
+
+      
+    Object.entries(OPTIONS)
+        .forEach(([options_type_name, options_of_type]) => {
+            wrapper_el.append(
+                get_options_type_text_el(options_type_name),
+                ...get_inputs_of_type(options_of_type)
+            )
+        })
 
     document.head.insertAdjacentHTML(
         'beforeend',
