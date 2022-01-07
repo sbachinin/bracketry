2a792dbdefaba682651452e836a553cbcd64f4fb
Sergey Bachinin <sbachinin@yandex.ru>
2022-01-07 20:52:04 +0400

    add seed, options: show_seed and seed_width

--- prepareMockData.mjs
+++ prepareMockData.mjs
@@ -13,6 +13,7 @@ const get_sides_data = (match_teams, all_data) => {
             isWinner: team.status === 'Winner',
             short_title: player_meta.short_name,
             nationality: player_meta.nationality.code,
+            seed: team_meta.seed
         }
     })
 }
