049e9ce29037c160713cbb793d0ab873cd475f47
Sergey Bachinin <sbachinin@yandex.ru>
2022-01-23 17:39:58 +0400

    disable incompatible options

--- options-inputs/options-inputs.mjs
+++ options-inputs/options-inputs.mjs
@@ -6,7 +6,7 @@ import { get_option_input } from './get-option-input.mjs'
 
 const names_of_expanded_groups = []
 
-const get_options_group_heading = (options_type_name, render_inputs) => {
+const get_options_group_heading = (options_type_name, update_inputs) => {
     const text = options_type_name
         .split('_')
         .filter(word => word !== 'OPTIONS')
@@ -45,13 +45,13 @@ const get_options_group_heading = (options_type_name, render_inputs) => {
         } else {
             names_of_expanded_groups.push(options_type_name)
         }
-        render_inputs()
+        update_inputs()
     })
 
     return el
 }
 
-const update_inputs = () => {
+const update_inputs = (options_to_values) => {
     Object.keys(OPTIONS).forEach(options_type_name => {
         document.querySelector(`.${options_type_name}-arrow`).style.transform = (
             names_of_expanded_groups.includes(options_type_name) ? 'rotate(180deg)' : 'none'
@@ -59,6 +59,14 @@ const update_inputs = () => {
         document.querySelector(`.${options_type_name}`).style.height = (
             names_of_expanded_groups.includes(options_type_name) ? 'auto' : 0
         )
+        Object.entries(OPTIONS[options_type_name]).forEach(([option_name, option_info]) => {
+            const wrapper_el = document.querySelector(`.${option_name}-input-wrapper`)
+            if (option_info.disable_if?.(options_to_values)) {
+                wrapper_el?.classList.add('disabled')
+            } else {
+                wrapper_el?.classList.remove('disabled')
+            }
+        })
     })
 }
 
@@ -83,7 +91,7 @@ const render_inputs = (data, user_options_to_values, wrapper_el, update_brackets
             })
         }
 
-        update_inputs()
+        update_inputs(options_to_values)
         update_brackets(data, options_to_values)
     }
 
@@ -91,7 +99,7 @@ const render_inputs = (data, user_options_to_values, wrapper_el, update_brackets
         const inputs = Object.entries(options)
             .map(([option_name, option_info]) => {
                 const option_wrapper_el = create_element_from_Html(
-                    `<div style='background: #ddff9b; padding: 7px;'>
+                    `<div class='option-input-wrapper ${option_name}-input-wrapper' style='background: #ddff9b; padding: 7px;'>
                         <p style='margin: 3px'>${escape_Html(option_info.title)}</p>
                     </div>`
                 )
@@ -122,12 +130,12 @@ const render_inputs = (data, user_options_to_values, wrapper_el, update_brackets
             wrapper_el.append(
                 get_options_group_heading(
                     options_type_name,
-                    update_inputs
+                    () => update_inputs(options_to_values)
                 ),
                 get_inputs_of_type(options_of_type, options_type_name)
             )
         })
-
+/* 
     wrapper_el.append(get_options_group_heading(
         'DATA_TEXTAREA',
         () => render_inputs(data, options_to_values, wrapper_el, update_brackets)
@@ -141,7 +149,7 @@ const render_inputs = (data, user_options_to_values, wrapper_el, update_brackets
             update_brackets(JSON.parse(e.target.value), options_to_values)
         })
         wrapper_el.append(data_textarea)
-    }
+    } */
 }
 
 export const get_options_inputs = (
@@ -163,7 +171,13 @@ export const get_options_inputs = (
 
     document.head.insertAdjacentHTML(
         'beforeend',
-        `<style>${switchStyle}</style>`
+        `<style>
+            ${switchStyle}
+            .option-input-wrapper.disabled > * {
+                opacity: 0.2;
+                pointer-events: none;
+            }
+        </style>`
     )
 
     return wrapper_el
