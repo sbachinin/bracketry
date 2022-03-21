8e70b92b1bd6653e100d5e1809529798cae5542b
Sergey Bachinin <sbachinin@yandex.ru>
2022-03-21 15:26:28 +0300

    protect against wrong get_flag_image_source: non-function, non-promise return value, non-CanvasImageSource payload

--- index.mjs
+++ index.mjs
@@ -4,14 +4,14 @@ import { prepareMockData } from './prepareMockData.mjs'
 import { get_options_inputs } from './options-inputs/options-inputs.mjs'
 
 const some_test_options = {
-    /* get_flag_image_source: nationality_code => {
+    get_flag_image_source: nationality_code => {
         return new Promise(resolve => {
             let img = new Image
             img.onload = () => resolve(img)
             img.onerror = () => resolve(null)
             img.src = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${nationality_code}.svg`
         })
-    } */
+    }
 }
 
 const canvas_container = document.getElementById('canvas-container')
@@ -19,6 +19,7 @@ const canvas_container = document.getElementById('canvas-container')
 fetch()
     .then(prepareMockData)
     .then(data => {
+        console.log(data)
         const update_brackets = createBrackets(
             data,
             canvas_container,
