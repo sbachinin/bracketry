9a1321cdb916a1244f807bd572d13d7f8660e92e
Sergey Bachinin <sbachinin@yandex.ru>
2022-04-18 17:19:36 +0300

    make 'match.order' required

--- mockFetchData.mjs
+++ mockFetchData.mjs
@@ -1,6 +1,7 @@
 import data1 from './ausopen-2022-live-data.mjs'
 import data2 from './ausopen-dot-com-data-2021.mjs'
+import data3 from './ausopen-2022-mens-singles-finished.mjs'
 
-window.mockData = data1
+window.mockData = data3
 
-export const mockFetchData = () => Promise.resolve(data1)
+export const mockFetchData = () => Promise.resolve(data3)
