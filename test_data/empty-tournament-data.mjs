89943039554404e08058cdba6c7c9c9ec753b15d
Sergey Bachinin <sbachinin@yandex.ru>
2022-05-28 20:37:08 +0300

    try empty data

---
+++ test_data/empty-tournament-data.mjs
@@ -0,0 +1,20 @@
+
+
+export default {
+    rounds: [
+        {
+            id: 'round1',
+            name: 'Ovo',
+        },
+        {
+            id: 'round2',
+            name: 'Fin',
+        }
+    ],
+
+    matches: [
+    ],
+
+    contestants: {
+    }
+}
--- test_data/get_some_data.mjs
+++ test_data/get_some_data.mjs
@@ -2,10 +2,11 @@ import data1 from './ausopen-2022-live-data.mjs'
 import data2 from './ausopen-dot-com-data-2021.mjs'
 import data3 from './ausopen-2022-mens-singles-finished.mjs'
 import data4 from './ausopen-2022-live-mens-doubles.mjs'
-import data5 from './test-tournament-data-1.mjs'
+import testdata1 from './test-tournament-data-1.mjs'
+import emptydata from './empty-tournament-data.mjs'
 import { prepareMockData } from './prepareMockData.mjs'
 
 export const get_some_data = () => {
-    // return Promise.resolve(data5)
+    // return Promise.resolve(testdata1)
     return prepareMockData(data3)
 }
