a1c50eaf5754d7c2ebdf13931a47af364ec2965a
Sergey Bachinin <sbachinin@yandex.ru>
2022-06-10 17:31:12 +0300

    refac buttons

--- index.mjs
+++ index.mjs
@@ -5,8 +5,10 @@ import { get_some_data } from './test_data/get_some_data.mjs'
 // import { create_external_buttons } from './external_hor_buttons_example.mjs'
 import { start_mock_matches_updates } from './start_mock_matches_updates.mjs'
 import { start_mock_full_data_updates } from './mock_full_data_updates.mjs'
+import { BUTTONS_POSITIONS } from './lib/constants.mjs'
 
 const some_test_options = {
+    horizontal_scroll_buttons_position: BUTTONS_POSITIONS.gutters
     // onMatchClick: (match) => {
     //     location.href = `/matches/${match.match_id}`
     // },
