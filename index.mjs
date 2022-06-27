1b7d5ac05f1dc6b23ec4f15ce0fc635684964ec2
Sergey Bachinin <sbachinin@yandex.ru>
2022-06-27 14:59:24 +0300

    add example of custom buttons as text

--- index.mjs
+++ index.mjs
@@ -2,10 +2,8 @@ import { createPlayoffs } from './lib/lib.mjs'
 import { get_some_data } from './test_data/get_some_data.mjs'
 import { start_mock_matches_updates } from './start_mock_matches_updates.mjs'
 import { start_mock_full_data_updates } from './mock_full_data_updates.mjs'
-import { BUTTONS_POSITIONS } from './lib/constants.mjs'
 
 const some_test_options = {
-    nav_buttons_position: BUTTONS_POSITIONS.gutters,
     // onMatchClick: (match) => {
     //     location.href = `/matches/${match.match_id}`
     // },
