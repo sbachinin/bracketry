76ac4394a7de6503131cb7242902d8ceefee39db
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-06 11:54:18 +0300

    test that code survives with invalid round data

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -40,3 +40,58 @@ test('survives if rounds array contains 0 elements', () => {
     )
     expect(true).toBe(true);
 });
+
+
+test('survives when non-object stuff is provided in rounds array', () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+
+    expect.assertions(1)
+
+    easyPlayoffs.createPlayoffs(
+        {
+            rounds: [3],
+            matches: [],
+            contestants: {}
+        },
+        wrapper,
+        {}
+    )
+    expect(true).toBe(true);
+});
+
+test('survives when empty round object is provided', () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+
+    expect.assertions(1)
+
+    easyPlayoffs.createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [],
+            contestants: {}
+        },
+        wrapper,
+        {}
+    )
+    expect(true).toBe(true);
+});
+
+test('survives when non-string id is provided for a round', () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+
+    expect.assertions(1)
+
+    easyPlayoffs.createPlayoffs(
+        {
+            rounds: [{id: true}],
+            matches: [],
+            contestants: {}
+        },
+        wrapper,
+        {}
+    )
+    expect(true).toBe(true);
+});
