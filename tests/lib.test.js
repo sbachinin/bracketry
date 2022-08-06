67036338a8063a8a38bc35d20e665eb9e0eb1df9
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-06 11:22:14 +0300

    test 'survives if no rounds array is provided'

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -4,7 +4,26 @@
 
 const { easyPlayoffs } = require('../index.js');
 
-test('survives empty data', () => {
+
+test('survives if no rounds array is provided', () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+
+    expect.assertions(1)
+
+    easyPlayoffs.createPlayoffs(
+        {
+            matches: [],
+            contestants: {}
+        },
+        wrapper,
+        {}
+    )
+    expect(true).toBe(true);
+});
+
+
+test('survives if rounds array contains 0 elements', () => {
     const wrapper = document.createElement('div')
     document.body.append(wrapper)
 
