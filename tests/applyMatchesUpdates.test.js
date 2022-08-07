0f12c5cb81a4e0779a15e8bfbc5aaaf1402bfd09
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-07 16:53:57 +0300

    test applyMatchesUpdates

---
+++ tests/applyMatchesUpdates.test.js
@@ -0,0 +1,171 @@
+/**
+ * @jest-environment jsdom
+ */
+
+global.ResizeObserver = require('resize-observer-polyfill')
+const { easyPlayoffs } = require('../index.js');
+
+const init = () => {
+    document.body.innerHTML = ''
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+    return wrapper
+}
+
+test('modifies score in existing match', () => {
+    const wrapper = init()
+
+    const { applyMatchesUpdates } = easyPlayoffs.createPlayoffs(
+        {
+            rounds: [
+                { id: 'round1', name: 'Some round' },
+            ],
+            matches: [
+                {
+                    match_id: '32323',
+                    round_id: 'round1',
+                    order: 0,
+                    sides: [
+                        { contestant_id: 'c1', score: [{ main_score: '4' }] },
+                    ]
+                }
+            ],
+            contestants: {
+                c1: {
+                    players: [
+                        {
+                            title: 'John Doe'
+                        }
+                    ]
+                },
+            }
+        },
+        wrapper,
+        {}
+    )
+
+
+    applyMatchesUpdates([
+        {
+            match_id: '32323',
+            round_id: 'round1',
+            order: 0,
+            sides: [
+                { contestant_id: 'c1', score: [{ main_score: '6' }] },
+            ]
+        }
+    ])
+
+    expect(document.querySelector('.main-score').innerHTML).toBe('6');
+
+});
+
+
+
+
+
+
+
+
+
+test('does not spoil a score in existing match if update is invalid', () => {
+    const wrapper = init()
+
+    const { applyMatchesUpdates } = easyPlayoffs.createPlayoffs(
+        {
+            rounds: [
+                { id: 'round1', name: 'Some round' },
+            ],
+            matches: [
+                {
+                    match_id: '32323',
+                    round_id: 'round1',
+                    order: 0,
+                    sides: [
+                        { contestant_id: 'c1', score: [{ main_score: '4' }] },
+                    ]
+                }
+            ],
+            contestants: {
+                c1: {
+                    players: [
+                        {
+                            title: 'John Doe'
+                        }
+                    ]
+                },
+            }
+        },
+        wrapper,
+        {}
+    )
+
+
+    applyMatchesUpdates([
+        {
+            match_id: '32323',
+            round_id: 'round1',
+            order: 0,
+            sides: [
+                { contestant_id: 'c1', score: [{ main_score: false }] },
+            ]
+        }
+    ])
+
+    expect(document.querySelector('.main-score').innerHTML).toBe('4');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test('creates new match', () => {
+    const wrapper = init()
+
+    const { applyMatchesUpdates } = easyPlayoffs.createPlayoffs(
+        {
+            rounds: [
+                { id: 'round1', name: 'Some round' },
+            ],
+            matches: [],
+            contestants: {
+                c1: {
+                    players: [
+                        {
+                            title: 'John Doe'
+                        }
+                    ]
+                },
+            }
+        },
+        wrapper,
+        {}
+    )
+
+    expect(document.querySelector('.main-score')).toBe(null);
+
+    applyMatchesUpdates([
+        {
+            match_id: '32323',
+            round_id: 'round1',
+            order: 0,
+            sides: [
+                { contestant_id: 'c1', score: [{ main_score: '6' }] },
+            ]
+        }
+    ])
+
+    expect(document.querySelector('.main-score').innerHTML).toBe('6');
+
+});
