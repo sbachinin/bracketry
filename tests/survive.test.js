2aec3c44b8a56fa83548b26d5c12d80dc86f5e90
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-08 18:46:46 +0300

    test with non-existent options

--- tests/survive.test.js
+++ tests/survive.test.js
@@ -705,3 +705,25 @@ test('survives non-object options', () => {
     )
     expect(true).toBe(true);
 })
+
+test('survives non-existent options', () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+
+    expect.assertions(1)
+
+    easyPlayoffs.createPlayoffs(
+        {
+            rounds: [{ id: 'r1' }],
+            matches: [{ match_id: '32323', round_id: 'r1', order: 0,
+                sides: [{ contestant_id: 'contestant1', score: [] }] }
+            ],
+            contestants: {
+                contestant1: { players: [ { title: 'fdf', nationality_code: 'fdsf', flag_url: false } ] }
+            }
+        },
+        wrapper,
+        { fsdfsd: false}
+    )
+    expect(true).toBe(true);
+})
