6ff911ed6f3386d6f5293186903166e34cba5d36
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-08 12:18:54 +0300

    Fix types for returned functions

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,64 @@
+import { Data, Match } from "./lib/data/data"
+
+type OptionNames = 'connectionLinesColor'
+    | 'connectionLinesWidth'
+    | 'defaultNavigationSvgSize'
+    | 'distanceBetweenScorePairs'
+    | 'highlightedConnectionLinesColor'
+    | 'highlightedPlayerTitleColor'
+    | 'leftNavigationButtonBackground'
+    | 'leftNavigationButtonHTML'
+    | 'liveMatchBackgroundColor'
+    | 'liveMatchBorderColor'
+    | 'mainBorderColor'
+    | 'mainVerticalPadding'
+    | 'matchAxisMargin'
+    | 'matchFontSize'
+    | 'matchHorMargin'
+    | 'matchMaxWidth'
+    | 'matchMinVerticalGap'
+    | 'matchTextColor'
+    | 'navButtonsPosition'
+    | 'navigationButtonsTopDistance'
+    | 'navigationGutterBorderColor'
+    | 'navigationSvgColor'
+    | 'onMatchClick'
+    | 'onMatchSideClick'
+    | 'oneSidePlayersGap'
+    | 'playerTitleFontFamily'
+    | 'rightNavigationButtonBackground'
+    | 'rightNavigationButtonHTML'
+    | 'rootBackgroundColor'
+    | 'rootFontFamily'
+    | 'roundTitleColor'
+    | 'roundTitlesBorderBottomColor'
+    | 'roundTitlesFontFamily'
+    | 'roundTitlesFontSize'
+    | 'roundTitlesHeight'
+    | 'scoreFontFamily'
+    | 'scrollbarColor'
+    | 'scrollbarWidth'
+    | 'showScrollbar'
+    | 'visibleRoundsCount'
+
+type OptionsMap = { [key in OptionNames]?: any }
+
 export function createPlayoffs(
-    user_data: import("./lib/data/data").Data,
+    user_data: Data,
     user_wrapper_el: any,
     user_options: any
 ): {
-    moveToPreviousRound: any;
-    moveToNextRound: any;
-    setBaseRoundIndex: any;
-    getNavigationState: any;
-    applyNewOptions: (new_options: any) => void;
-    applyFullDataUpdate: (new_data: any) => void;
-    applyMatchesUpdates: (matches_data: import("./lib/data/data").Match[]) => void;
-    getAllData: () => any;
-    getUserOptions: any;
-    applyAllData?: undefined;
+    moveToNextRound: () => void;
+    moveToPreviousRound: () => void;
+    setBaseRoundIndex: (i: number) => void;
+    getNavigationState: () => {
+        reachedRightEdge: boolean,
+        allRoundsAreVisible: boolean,
+        baseRoundIndex: number
+    }
+    applyNewOptions: (new_options: OptionsMap) => void;
+    applyFullDataUpdate: (new_data: Data) => void;
+    applyMatchesUpdates: (matches_data: Match[]) => void;
+    getAllData: () => Data;
+    getUserOptions: () => OptionsMap;
 };
