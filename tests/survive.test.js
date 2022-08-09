b96bdf4f889d9e744bb347a750263821b79ff5c9
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-09 11:38:48 +0300

    test when no contestants

--- tests/survive.test.js
+++ tests/survive.test.js
@@ -23,6 +23,22 @@ test('survives if no rounds array is provided', () => {
     expect(true).toBe(true);
 });
 
+test('survives no contestants', () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+
+    expect.assertions(1)
+
+    easyPlayoffs.createPlayoffs(
+        {
+            matches: [],
+        },
+        wrapper,
+        {}
+    )
+    expect(true).toBe(true);
+});
+
 
 test('survives if rounds array contains 0 elements', () => {
     const wrapper = document.createElement('div')
