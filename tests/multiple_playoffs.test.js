8c54fc19ebd2cd1432b9c430156a8c5eb7c75a6f
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-19 21:24:49 +0500

    halt analyze_data if data is not an object or "rounds" are missing or empty

--- tests/multiple_playoffs.test.js
+++ tests/multiple_playoffs.test.js
@@ -2,20 +2,20 @@
  * @jest-environment jsdom
  */
 
- global.ResizeObserver = require('resize-observer-polyfill')
- const { createPlayoffs } = require('../index.js').easyPlayoffs
- const finished_ucl = require('./ucl-finished.js').default
-  
- const create_wrapper = () => {
-     const wrapper = document.createElement('div')
-     document.body.append(wrapper)
-     return wrapper
- }
- 
+global.ResizeObserver = require('resize-observer-polyfill')
+const { createPlayoffs } = require('../index.js').easyPlayoffs
+const finished_ucl = require('./ucl-finished.js').default
+
+const create_wrapper = () => {
+    const wrapper = document.createElement('div')
+    document.body.append(wrapper)
+    return wrapper
+}
+
 
 test('replaces old playoffs with new playoffs when createPlayoffs is called again with the same wrapper', () => {
     const wrapper = create_wrapper()
-    
+
     const first_data = {
         rounds: [{}],
         matches: [{
@@ -33,11 +33,11 @@ test('replaces old playoffs with new playoffs when createPlayoffs is called agai
         }
     }
 
-    const second_data = { rounds: [{}, {}, {}, {} ] }
+    const second_data = { rounds: [{}, {}, {}, {}] }
 
     createPlayoffs(first_data, wrapper)
     createPlayoffs(second_data, wrapper)
-    
+
     expect(wrapper.querySelectorAll('.root-brackets-element').length).toBe(1)
     expect(wrapper.querySelectorAll('.round-wrapper').length).toBe(4)
     expect(wrapper.querySelectorAll('.side-wrapper[contestant-id]').length).toBe(0)
@@ -48,10 +48,10 @@ test('replaces old playoffs with new playoffs when createPlayoffs is called agai
 test(`two coexisting playoffs don't intervene into each other's navigation state`, () => {
     const wrapper1 = create_wrapper()
     const wrapper2 = create_wrapper()
-    
-    const pl1 = createPlayoffs({ rounds: [{}, {}, {}, {}, {} ] }, wrapper1, { visibleRoundsCount: 1 })
-    const pl2 = createPlayoffs({ rounds: [{}, {}, {}, {} ] }, wrapper2, { visibleRoundsCount: 2 })
-    
+
+    const pl1 = createPlayoffs({ rounds: [{}, {}, {}, {}, {}] }, wrapper1, { visibleRoundsCount: 1 })
+    const pl2 = createPlayoffs({ rounds: [{}, {}, {}, {}] }, wrapper2, { visibleRoundsCount: 2 })
+
     pl1.moveToLastRound()
     expect(pl1.getNavigationState().baseRoundIndex).toBe(4)
     expect(pl2.getNavigationState().baseRoundIndex).toBe(0)
@@ -61,10 +61,10 @@ test(`two coexisting playoffs don't intervene into each other's navigation state
 test(`two coexisting playoffs don't intervene into each other's data`, () => {
     const wrapper1 = create_wrapper()
     const wrapper2 = create_wrapper()
-    
-    const pl1 = createPlayoffs({ rounds: [{}, {}, {}, {}, {} ] }, wrapper1)
-    const pl2 = createPlayoffs({ rounds: [{}, {}, {}, {} ] }, wrapper2)
-    
+
+    const pl1 = createPlayoffs({ rounds: [{}, {}, {}, {}, {}] }, wrapper1)
+    const pl2 = createPlayoffs({ rounds: [{}, {}, {}, {}] }, wrapper2)
+
     pl1.replaceData({ rounds: [{}] })
     expect(pl1.getAllData().rounds.length).toBe(1)
     expect(pl2.getAllData().rounds.length).toBe(4)
@@ -74,10 +74,10 @@ test(`two coexisting playoffs don't intervene into each other's data`, () => {
 test(`two coexisting playoffs don't intervene into each other's "highlighted" state`, () => {
     const wrapper1 = create_wrapper()
     const wrapper2 = create_wrapper()
-    
+
     createPlayoffs(finished_ucl, wrapper1)
     createPlayoffs(finished_ucl, wrapper2)
-    
+
     const benfica_side_selector = `.side-wrapper[contestant-id='benfica']`
 
     wrapper1.querySelector(benfica_side_selector)
@@ -92,10 +92,10 @@ test(`two coexisting playoffs don't intervene into each other's "highlighted" st
 
 test('calls "dead" playoffs api functions after it being removed from the DOM', () => {
     const wrapper = create_wrapper()
-    
+
     const old_pl = createPlayoffs({ rounds: [] }, wrapper)
     const new_pl = createPlayoffs({ rounds: [] }, wrapper)
-    
+
     // new_pl.getNavigationState()
 })
 
