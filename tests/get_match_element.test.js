28aed155bdeef70cf67a4c708f7b98f7c1e55d3f
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-20 18:54:33 +0500

    add more get_match_element tests

--- tests/get_match_element.test.js
+++ tests/get_match_element.test.js
@@ -6,8 +6,7 @@ global.ResizeObserver = require('resize-observer-polyfill')
 const { createPlayoffs } = require('../index.js').easyPlayoffs
 const finished_ucl = require('./ucl-finished.js').default
 
-const init = () => {
-    document.body.innerHTML = ''
+const create_wrapper = () => {
     const wrapper = document.createElement('div')
     document.body.append(wrapper)
     return wrapper
@@ -15,7 +14,7 @@ const init = () => {
 
 
 test('can draw asymmetrical scores where two sides have different number of elements in score array', () => {
-    const wrapper = init()
+    const wrapper = create_wrapper()
 
     createPlayoffs(
         {
@@ -43,7 +42,7 @@ test('can draw asymmetrical scores where two sides have different number of elem
 
 
 test('can draw asymmetrical scores where ONLY ONE SIDE has a score array', () => {
-    const wrapper = init()
+    const wrapper = create_wrapper()
 
     createPlayoffs(
         {
@@ -70,18 +69,246 @@ test('can draw asymmetrical scores where ONLY ONE SIDE has a score array', () =>
 })
 
 
+test('renders a contentful match even if "contestants" are undefined', () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{
+                id: '32323',
+                round_index: 0,
+                order: 0,
+                sides: [{ contestant_id: 'abc' }],
+                match_status: 'Scheduled'
+            }],
+        },
+        wrapper,
+    )
+    expect(wrapper.querySelector('.match-status').textContent).toBe('Scheduled')
+});
+
+
+test(`renders a contentful match even if "contestants" don't contain such contestant_id`, () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{
+                id: '32323',
+                round_index: 0,
+                order: 0,
+                sides: [{ contestant_id: 'abc' }],
+                match_status: 'Scheduled'
+            }],
+            contestants: {
+                c1: { players: [{ title: 'Josh' }] }
+            }
+        },
+        wrapper,
+    )
+    expect(wrapper.querySelector('.match-status').textContent).toBe('Scheduled')
+})
+
+test('renders a contentful match even if match.sides is undefined', () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0, match_status: 'Scheduled' }],
+        },
+        wrapper
+    )
+    expect(wrapper.querySelector('.match-status').textContent).toBe('Scheduled')
+})
+
+test('renders a contentful match even if match.sides is an empty array', () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0, sides: [], match_status: 'Scheduled' }],
+        },
+        wrapper
+    )
+    expect(wrapper.querySelector('.match-status').textContent).toBe('Scheduled')
+})
 
 
 
-// draws a winner mark for a side which has { is_winner: true }
-// does not draw a winner mark when neither side has { is_winner: true }
+test('does not render .sides element if match.sides is undefined', () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0 }],
+        },
+        wrapper
+    )
+    expect(wrapper.querySelector('.match-body .sides')).toBe(null)
+})
+
+test('does not render .sides element if match.sides is an empty array', () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0, sides: [] }],
+        },
+        wrapper
+    )
+    expect(wrapper.querySelector('.match-body .sides')).toBe(null)
+})
+
+
+test('renders .sides element if match.sides array contains at least one item', () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0, sides: [{}] }],
+        },
+        wrapper
+    )
+    expect(wrapper.querySelector('.match-body .sides')).not.toBe(null)
+})
+
+test(`renders two .side-wrapper elements even if match.sides contains only 1 side`, () => {
+    // (two sides are necessary for vertical alignment)
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0, sides: [{}] }],
+        },
+        wrapper
+    )
+    expect(wrapper.querySelectorAll('.match-body .side-wrapper').length).toBe(2)
+})
+
+
+test(`allow clicks on a .side-wrapper which has a contestant_id (even if no contestant data for such id)`, () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{
+                id: 'm1', round_index: 0, order: 0,
+                sides: [{ contestant_id: 'c1' }]
+            }],
+        },
+        wrapper
+    )
+
+    expect(
+        getComputedStyle(
+            wrapper.querySelector('.side-wrapper[contestant-id="c1"]')
+        ).pointerEvents
+    ).toBe('auto')
+})
+
+
+test(`forbid clicks on a side-wrapper which has no contestant_id`, () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{ id: 'm1', round_index: 0, order: 0, sides: [{ contestant_id: 'c1' }] }],
+        },
+        wrapper
+    )
+
+    expect(
+        getComputedStyle(
+            wrapper.querySelectorAll('.side-wrapper')[1]
+        ).pointerEvents
+    ).toBe('none')
+})
 
 
 
-// does not render nationalies if not provided
-// does not render entry statuses if not provided
+test(`does not render a .score element for a side which has a 0-length "score" array`, () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{
+                id: 'm1', round_index: 0, order: 0, sides: [
+                    {
+                        contestant_id: 'c1',
+                        score: [{ main_score: 'Walkover' }]
+                    },
+                    {
+                        contestant_id: 'c2',
+                        score: []
+                    }
+                ]
+            }],
+        },
+        wrapper
+    )
+
+    expect(
+        wrapper.querySelector('.side-wrapper[contestant-id="c2"] .score')
+    ).toBe(null)
+})
 
 
-// subscore
 
-// match_status OR score
+test(`draws a score even if side has no "contestant_id"`, () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{
+                id: 'm1', round_index: 0, order: 0, sides: [
+                    {
+                        score: [{ main_score: 'Walkover' }]
+                    }
+                ]
+            }],
+        },
+        wrapper
+    )
+
+
+    expect(
+        wrapper.querySelector('.side-wrapper:first-of-type .side-own-score').textContent.trim()
+    ).toBe('Walkover')
+})
+
+
+
+test(`draws a score even if contestant not found for such side`, () => {
+    const wrapper = create_wrapper()
+
+    createPlayoffs(
+        {
+            rounds: [{}],
+            matches: [{
+                id: 'm1', round_index: 0, order: 0, sides: [
+                    {
+                        contestant_id: 'c1',
+                        score: [{ main_score: 'Walkover' }]
+                    }
+                ]
+            }],
+        },
+        wrapper
+    )
+
+    expect(
+        wrapper.querySelector('.side-wrapper[contestant-id="c1"] .side-own-score').textContent.trim()
+    ).toBe('Walkover')
+})
