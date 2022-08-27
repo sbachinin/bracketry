a4cade55c62fb722691425404c35631626167f78
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-27 17:56:33 +0500

    add tests that options fall back to default values

--- tests/options.test.js
+++ tests/options.test.js
@@ -6,57 +6,31 @@ global.ResizeObserver = require('resize-observer-polyfill')
 const { easyPlayoffs } = require('../index.js');
 const finished_ucl = require('./ucl-finished.js').default
 
-const init = () => {
-    document.body.innerHTML = ''
-    const wrapper = document.createElement('div')
-    document.body.append(wrapper)
-    return wrapper
-}
+const { init } = require('./utils.js')
 
 
 test('tells user options via getUserOptions', () => {
-    const wrapper = init()
-
-    const { getUserOptions } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    expect(getUserOptions()).toEqual({ visibleRoundsCount: 2 })
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    expect(pl.getUserOptions()).toEqual({ visibleRoundsCount: 2 })
 })
 
 test('applies new visibleRoundsCount via applyNewOptions', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { applyNewOptions, getUserOptions } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
+    pl.applyNewOptions({ visibleRoundsCount: 4 })
 
-    applyNewOptions({ visibleRoundsCount: 4 })
-
-    const all_rounds = [...document.querySelectorAll('.round-wrapper')]
+    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
     const visible_rounds = all_rounds.filter(w => !w.classList.contains('hidden'))
     expect(visible_rounds.length).toBe(4)
-    expect(getUserOptions()).toMatchObject({ visibleRoundsCount: 4 })
+    expect(pl.getUserOptions()).toMatchObject({ visibleRoundsCount: 4 })
 })
 
 
 test('merges old and new options', () => {
-    const wrapper = init()
-
-    const { applyNewOptions, getUserOptions } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    applyNewOptions({ matchMinVerticalGap: 4 })
-
-    expect(getUserOptions().visibleRoundsCount).toBe(2)
-    expect(getUserOptions().matchMinVerticalGap).toBe(4)
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    pl.applyNewOptions({ matchMinVerticalGap: 4 })
+    expect(pl.getUserOptions().visibleRoundsCount).toBe(2)
+    expect(pl.getUserOptions().matchMinVerticalGap).toBe(4)
 })
 
 
@@ -64,17 +38,54 @@ test('merges old and new options', () => {
 
 
 test('applies matchMaxWidth to .match-body', () => {
-    const wrapper = init()
-
-    easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { matchMaxWidth: 250 }
-    )
-
+    const { wrapper } = init(finished_ucl, { matchMaxWidth: 250 })
     expect(
-        getComputedStyle(document.querySelector('.match-body')).maxWidth
+        getComputedStyle(wrapper.querySelector('.match-body')).maxWidth
     ).toBe('250px')
+})
+
+
+
+test(`falls back to default options in case of EMPTY options`, () => {
+    const { wrapper } = init(finished_ucl)
+    expect(
+        getComputedStyle(wrapper.querySelector('.round-titles-wrapper')).height
+    ).toBe('50px')
+    expect(
+        getComputedStyle(wrapper.querySelector('.match-wrapper')).paddingLeft
+    ).toBe('20px')
+    expect(
+        getComputedStyle(wrapper.querySelector('.match-body')).maxWidth
+    ).toBe('450px')
+})
+
+
+test(`falls back to default option if NaN is provided for an option of type "number"`, () => {
+    const { wrapper } = init(finished_ucl, {
+        roundTitlesHeight: NaN, matchHorMargin: NaN
+    })
+    expect(
+        getComputedStyle(wrapper.querySelector('.round-titles-wrapper')).height
+    ).toBe('50px')
+    expect(
+        getComputedStyle(wrapper.querySelector('.match-wrapper')).paddingLeft
+    ).toBe('20px')
+})
+
+
+test(`falls back to default options in case of INVALID options`, () => {
+    const { wrapper } = init(finished_ucl, {
+        roundTitlesHeight: {}, matchHorMargin: [], matchMaxWidth: 'crap'
+    })
+    expect(
+        getComputedStyle(wrapper.querySelector('.round-titles-wrapper')).height
+    ).toBe('50px')
+    expect(
+        getComputedStyle(wrapper.querySelector('.match-wrapper')).paddingLeft
+    ).toBe('20px')
+    expect(
+        getComputedStyle(wrapper.querySelector('.match-body')).maxWidth
+    ).toBe('450px')
 })
 
 // TODO other heavy-logic options
