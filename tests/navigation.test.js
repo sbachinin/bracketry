d7b44e95b531b10fad89098563c7a56c33000dc8
Sergey Bachinin <sbachinin@yandex.ru>
2022-09-07 21:20:15 +0500

    improve navigation tests

--- tests/navigation.test.js
+++ tests/navigation.test.js
@@ -3,26 +3,13 @@
  */
 
 global.ResizeObserver = require('resize-observer-polyfill')
-const { easyPlayoffs } = require('../index.js');
+const { init } = require('./utils.js')
 const finished_ucl = require('./ucl-finished.js').default
 
-const init = () => {
-    document.body.innerHTML = ''
-    const wrapper = document.createElement('div')
-    document.body.append(wrapper)
-    return wrapper
-}
-
 test('hides a number of rounds beyond options.visibleRoundsCount', () => {
-    const wrapper = init()
+    const { wrapper } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    const all_rounds = [...document.querySelectorAll('.round-wrapper')]
+    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
     const hidden_rounds = all_rounds.filter(w => w.classList.contains('hidden'))
     expect(hidden_rounds.length).toBe(2)
     expect(getComputedStyle(all_rounds[0]).display).not.toBe('none')
@@ -32,70 +19,37 @@ test('hides a number of rounds beyond options.visibleRoundsCount', () => {
 
 
 test('floors the fractional visibleRoundsCount', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2.5 })
 
-    const { setBaseRoundIndex, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2.5 }
-    )
-
-    const all_rounds = [...document.querySelectorAll('.round-wrapper')]
+    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
     const visible_rounds = all_rounds.filter(w => !w.classList.contains('hidden'))
     expect(visible_rounds.length).toBe(2)
 
-    setBaseRoundIndex(2)
-    expect(getNavigationState().baseRoundIndex).toBe(2)
+    pl.setBaseRoundIndex(2)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(2)
 })
 
 
 
 
-test('survives with negative visibleRoundsCount', () => {
-    const wrapper = init()
+test('renders content with negative visibleRoundsCount', () => {
     expect.assertions(1)
-
-    easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: -2.5 }
-    )
-
-    expect(true).toBe(true);
+    const { wrapper } = init(finished_ucl, { visibleRoundsCount: -2.5 })
+    expect(wrapper.querySelectorAll('.round-wrapper').length).toBe(4)
 })
 
 
 test('shows more rounds when a greater visibleRoundsCount is supplied via applyNewOptions', () => {
-    const wrapper = init()
-
-    const { applyNewOptions } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    applyNewOptions({ visibleRoundsCount: 4 })
-
-    expect(
-        document.querySelectorAll('.round-wrapper:not(.hidden)').length
-    ).toBe(4)
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    pl.applyNewOptions({ visibleRoundsCount: 4 })
+    expect(wrapper.querySelectorAll('.round-wrapper:not(.hidden)').length).toBe(4)
 })
 
 
 test('shows less rounds when a lesser visibleRoundsCount is supplied via applyNewOptions', () => {
-    const wrapper = init()
-
-    const { applyNewOptions } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 4 }
-    )
-
-    applyNewOptions({ visibleRoundsCount: 2 })
-
-    expect(
-        document.querySelectorAll('.round-wrapper:not(.hidden)').length
-    ).toBe(2)
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 4 })
+    pl.applyNewOptions({ visibleRoundsCount: 2 })
+    expect(wrapper.querySelectorAll('.round-wrapper:not(.hidden)').length).toBe(2)
 })
 
 
@@ -104,210 +58,139 @@ test(`shows more rounds when
     a) a greater visibleRoundsCount is supplied via applyNewOptions
     and b) navigation is at the very right
 `, () => {
-    const wrapper = init()
-
-    const { applyNewOptions, setBaseRoundIndex } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    setBaseRoundIndex(2)
-    applyNewOptions({ visibleRoundsCount: 3 })
-
-    expect(
-        document.querySelectorAll('.round-wrapper:not(.hidden)').length
-    ).toBe(3)
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    pl.setBaseRoundIndex(2)
+    pl.applyNewOptions({ visibleRoundsCount: 3 })
+    expect(wrapper.querySelectorAll('.round-wrapper:not(.hidden)').length).toBe(3)
 })
 
 
 
 test('sets the base round index + tells this index via getNavigationState', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { setBaseRoundIndex, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    setBaseRoundIndex(2)
-    expect(getNavigationState().baseRoundIndex).toBe(2)
+    pl.setBaseRoundIndex(2)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(2)
 
     // first round now should be hidden:
-    const all_rounds = [...document.querySelectorAll('.round-wrapper')]
+    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
     expect(getComputedStyle(all_rounds[0]).display).toBe('none')
 })
 
 
 
 test('limits the base round index when setBaseRoundIndex is called with invalid number', () => {
-    const wrapper = init()
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { setBaseRoundIndex, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    setBaseRoundIndex(235152)
-    expect(getNavigationState().baseRoundIndex).toBe(2);
-    setBaseRoundIndex(-23123)
-    expect(getNavigationState().baseRoundIndex).toBe(0);
+    pl.setBaseRoundIndex(235152)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(2);
+    pl.setBaseRoundIndex(-23123)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(0);
 })
 
 
 
 test('moves to next round when "moveToNextRound" is called', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { moveToNextRound, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
+    pl.moveToNextRound()
+    expect(pl.getNavigationState().baseRoundIndex).toBe(1)
 
-    moveToNextRound()
-    expect(getNavigationState().baseRoundIndex).toBe(1)
-
-    const all_rounds = [...document.querySelectorAll('.round-wrapper')]
+    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
     expect(getComputedStyle(all_rounds[0]).display).toBe('none')
     expect(getComputedStyle(all_rounds[2]).display).not.toBe('none')
 })
 
 
 test('moves to next round when right button is clicked', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    document.querySelector('.navigation-button.non-header-button.right')
+    wrapper.querySelector('.navigation-button.non-header-button.right')
         .dispatchEvent(new MouseEvent('mouseup', { bubbles: true }))
 
-    expect(getNavigationState().baseRoundIndex).toBe(1)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(1)
 
-    const all_rounds = [...document.querySelectorAll('.round-wrapper')]
+    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
     expect(getComputedStyle(all_rounds[0]).display).toBe('none')
     expect(getComputedStyle(all_rounds[2]).display).not.toBe('none')
 })
 
 
 test('moves to previous round', () => {
-    const wrapper = init()
-
-    const { moveToNextRound, moveToPreviousRound, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    moveToNextRound()
-    moveToPreviousRound()
-    expect(getNavigationState().baseRoundIndex).toBe(0)
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    pl.moveToNextRound()
+    pl.moveToPreviousRound()
+    expect(pl.getNavigationState().baseRoundIndex).toBe(0)
 })
 
 
 test('moves next only to the last possible round', () => {
-    const wrapper = init()
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { moveToNextRound, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
+    pl.moveToNextRound()
+    pl.moveToNextRound()
+    pl.moveToNextRound()
+    pl.moveToNextRound()
+    pl.moveToNextRound()
+    pl.moveToNextRound()
+    pl.moveToNextRound()
+    pl.moveToNextRound()
 
-    moveToNextRound()
-    moveToNextRound()
-    moveToNextRound()
-    moveToNextRound()
-    moveToNextRound()
-    moveToNextRound()
-    moveToNextRound()
-    moveToNextRound()
-
-    expect(getNavigationState().baseRoundIndex).toBe(2)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(2)
 })
 
 
 test('moves to last round when moveToLastRound is called', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
 
-    const { moveToLastRound, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
+    pl.moveToLastRound()
 
-    moveToLastRound()
-
-    expect(getNavigationState().baseRoundIndex).toBe(2)
-    expect(getNavigationState().reachedRightEdge).toBe(true)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(2)
+    expect(pl.getNavigationState().reachedRightEdge).toBe(true)
     expect(
-        getComputedStyle(document.querySelector('.round-wrapper:last-of-type')).display
+        getComputedStyle(wrapper.querySelector('.round-wrapper:last-of-type')).display
     ).not.toBe('none')
 })
 
 
 test('tells that it reached right edge when it is so', () => {
-    const wrapper = init()
-
-    const { moveToNextRound, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 2 }
-    )
-
-    moveToNextRound()
-    expect(getNavigationState().reachedRightEdge).toBe(false)
-    moveToNextRound()
-    expect(getNavigationState().reachedRightEdge).toBe(true)
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    pl.moveToNextRound()
+    expect(pl.getNavigationState().reachedRightEdge).toBe(false)
+    pl.moveToNextRound()
+    expect(pl.getNavigationState().reachedRightEdge).toBe(true)
 })
 
 
 
 
 test('does nothing on navigation-buttons click if all rounds are visible', () => {
-    const wrapper = init()
+    const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 4 })
 
-    const { getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 4 }
-    )
-
-    document.querySelector('.navigation-button.non-header-button.right')
+    wrapper.querySelector('.navigation-button.non-header-button.right')
         .dispatchEvent(new MouseEvent('mouseup', { bubbles: true }))
 
-    expect(getNavigationState().baseRoundIndex).toBe(0)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(0)
 })
 
 
 
 test('ignores setBaseRoundIndex() if all rounds are visible', () => {
-    const wrapper = init()
-
-    const { setBaseRoundIndex, getNavigationState } = easyPlayoffs.createPlayoffs(
-        finished_ucl,
-        wrapper,
-        { visibleRoundsCount: 4 }
-    )
-
-    setBaseRoundIndex(4)
-
-    expect(getNavigationState().baseRoundIndex).toBe(0)
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 4 })
+    pl.setBaseRoundIndex(4)
+    expect(pl.getNavigationState().baseRoundIndex).toBe(0)
 })
-
-// TODO getNavigationState tells if allRoundAreVisible
-
 
 
 test(`returns empty object if getNavigationState is called after elements were removed`, () => {
-    const wrapper = init()
-    const { getNavigationState } = easyPlayoffs.createPlayoffs(finished_ucl, wrapper)
+    const { wrapper, playoffs: pl } = init(finished_ucl)
     wrapper.remove()
-    expect(getNavigationState()).toEqual({})
+    expect(pl.getNavigationState()).toEqual({})
+})
+
+test(`getNavigationState tells if allRoundsAreVisible`, () => {
+    const { playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 4 })
+    expect(pl.getNavigationState().allRoundsAreVisible).toBe(true)
+    pl.applyNewOptions({ visibleRoundsCount: 2 })
+    expect(pl.getNavigationState().allRoundsAreVisible).toBe(false)
 })
