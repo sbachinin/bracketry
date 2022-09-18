86dfab8ce09e6ff01bd70d6b5db09c28aec3bc2f
Sergey Bachinin <sbachinin@yandex.ru>
2022-09-18 21:23:21 +0500

    improve options

--- tests/options.test.js
+++ tests/options.test.js
@@ -28,13 +28,72 @@ test(`applies options.matchMaxWidth to .match-body`, () => {
 test(`applies new visibleRoundsCount via applyNewOptions`, () => {
 
     const { wrapper, playoffs: pl } = init(finished_ucl, { visibleRoundsCount: 2 })
+    pl.applyNewOptions({ visibleRoundsCount: 1 })
+    expect(wrapper.querySelector('.content-horizontal-scroller').style.width).toBe('400%')
+})
 
-    pl.applyNewOptions({ visibleRoundsCount: 4 })
 
-    const all_rounds = [...wrapper.querySelectorAll('.round-wrapper')]
-    const visible_rounds = all_rounds.filter(w => !w.classList.contains('hidden'))
-    expect(visible_rounds.length).toBe(4)
-    expect(pl.getUserOptions()).toMatchObject({ visibleRoundsCount: 4 })
+
+test(`does not apply invalid options supplied by applyNewOptions`, () => {
+
+    const { wrapper, playoffs: pl } = init(finished_ucl, { roundTitlesHeight: 38 })
+
+    pl.applyNewOptions({ roundTitlesHeight: 'poop', scrollbarWidth: NaN })
+
+    expect(
+        getComputedStyle(wrapper.querySelector('.round-titles-wrapper')).height
+    ).toBe('38px')
+
+    expect(
+        getComputedStyle(wrapper.querySelector('.scrollbar-parent')).width
+    ).toBe('5px')
+})
+
+
+
+test(`getUserOptions returns only valid options supplied to createPlayoffs`, () => {
+
+    const { playoffs: pl } = init(finished_ucl, {
+        roundTitlesHeight: 'poop',
+        scrollbarWidth: NaN,
+        visibleRoundsCount: 2
+    })
+
+    expect(pl.getUserOptions()).toEqual({ visibleRoundsCount: 2 })
+})
+
+
+
+test(`getUserOptions returns only valid options supplied by applyNewOptions`, () => {
+
+    const { playoffs: pl } = init(finished_ucl, { roundTitlesHeight: 38 })
+
+    pl.applyNewOptions({ roundTitlesHeight: 'poop', scrollbarWidth: NaN, visibleRoundsCount: 2 })
+
+    expect(pl.getUserOptions()).toMatchObject({ roundTitlesHeight: 38, visibleRoundsCount: 2 })
+})
+
+
+
+test(`falls back to default options in case of non-object options`, () => {
+
+    const { wrapper, playoffs: pl } = init(finished_ucl, 'i am an idiot')
+    expect(
+        getComputedStyle(wrapper.querySelector('.round-titles-wrapper')).height
+    ).toBe('50px')
+    expect(pl.getUserOptions()).toEqual({})
+})
+
+
+
+test(`ignores non-object options passed to applyNewOptions`, () => {
+
+    const { wrapper, playoffs: pl } = init(finished_ucl)
+    pl.applyNewOptions('i am an idiot')
+    expect(
+        getComputedStyle(wrapper.querySelector('.round-titles-wrapper')).height
+    ).toBe('50px')
+    expect(pl.getUserOptions()).toEqual({})
 })
 
 
@@ -114,7 +173,7 @@ test(`ignores mutations of object returned by getUserOptions`, () => {
 
     const options = { roundTitlesHeight: 30 }
     const { wrapper, playoffs: pl } = init(finished_ucl, options)
-    
+
     const returned_options = pl.getUserOptions()
     returned_options.roundTitlesHeight = 5000
 
@@ -127,10 +186,8 @@ test(`ignores mutations of object returned by getUserOptions`, () => {
 
 
 test(`getUserOptions returns object which is a merge of initial options and those changed by applyNewOptions`, () => {
-    
-    const { playoffs: pl } = init(finished_ucl, { roundTitlesHeight: 30 })
-    pl.applyNewOptions({ matchMaxWidth: 333 })
-    expect(pl.getUserOptions()).toEqual({ roundTitlesHeight: 30, matchMaxWidth: 333 })
-})
 
-// TODO does not mutate an options object supplied by a user
+    const { playoffs: pl } = init(finished_ucl, { roundTitlesHeight: 30 })
+    pl.applyNewOptions({ matchMaxWidth: '333px' })
+    expect(pl.getUserOptions()).toEqual({ roundTitlesHeight: 30, matchMaxWidth: '333px' })
+})
