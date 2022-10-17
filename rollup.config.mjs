c2b3373c6b9eee3b0d36fce0092261c354ae0b17
Sergey Bachinin <sbachinin@yandex.ru>
2022-10-17 19:48:35 +0500

    use scss for permanent stlyles
    and remove css comments on build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,34 @@
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import { terser } from "rollup-plugin-terser"
+import scss from 'rollup-plugin-scss'
+import stripCssComments from 'strip-css-comments'
+// import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 
 export default [
-  {
-    input: "./lib/lib.mjs",
-    output: [
-      {
-        file: "dist/cjs/index.js",
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: "dist/esm/index.js",
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      terser()
-    ]
-  }
+    {
+        input: "./lib/lib.mjs",
+        output: [
+            {
+                file: "dist/cjs/index.js",
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: "dist/esm/index.js",
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            terser(),
+            // getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
+            scss({
+                output: false,
+                processor: stripCssComments
+            })
+        ]
+    }
 ];
