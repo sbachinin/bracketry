8ee9c00b4f1968a7257cf85639ef5bfbd05d08f9
Sergey Bachinin <sbachinin@yandex.ru>
2022-10-28 16:36:37 +0500

    auto set verticalScrollMode to 'buttons' on fullscreen

--- tests/vertical_scroll.test.js
+++ tests/vertical_scroll.test.js
@@ -1,7 +1,16 @@
-// TODO test: if nonsense provided to this option, make sure that vertical scroll falls back to mousewheel
+/**
+ * @jest-environment jsdom
+ */
 
+global.ResizeObserver = require('resize-observer-polyfill')
+const { init } = require('./utils.js')
+const finished_ucl = require('./ucl-finished.js').default
 
+// TODO
+test('if verticalScrollMode is set to nonsense, it falls back to mousewheel', () => {
+    //  let { wrapper, playoffs: pl } = init(finished_ucl)
 
+})
 
 
 // TODO test: resets scroll on navigation with classical layout
