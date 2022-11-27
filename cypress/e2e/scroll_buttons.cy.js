52d71674cf2471cf436d31949f85fbe20edfc199
Sergey Bachinin <sbachinin@yandex.ru>
2022-11-27 12:41:55 +0530

    fix scrollbuttons padding & test

--- cypress/e2e/scroll_buttons.cy.js
+++ cypress/e2e/scroll_buttons.cy.js
@@ -1,24 +1,41 @@
 import { get_query } from './get_query.js'
 
 describe('Vertical scroll buttons', () => {
-    
+
     it(`Scroll buttons are hidden with "native" verticalScrollMode`, () => {
         cy.visit(`http://localhost:3000`)
         cy.get('.button-up').should('not.be.visible')
+        cy.get('.button-down').should('not.be.visible')
     })
 
-    it(`Scroll buttons are visible with "buttons" verticalScrollMode`, () => {
+    it(`Scroll buttons are visible and clickable with "buttons" verticalScrollMode`, () => {
         cy.visit(`http://localhost:3000?${get_query({
-            verticalScrollMode: 'buttons'
+            verticalScrollMode: 'buttons',
+            syntheticScrollAmount: 200
         })}`)
-        cy.get('.button-up').should('be.visible')
+        cy.get('.button-up')
+            .should('be.visible')
+
+        cy.get('.button-down')
+            .should('be.visible')
+            .click()
+
+        cy.get('.matches-positioner').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 0, -200)')
     })
 
-    it(`Scroll buttons are visible with "mixed" verticalScrollMode`, () => {
+    it(`Scroll buttons are visible and clickable with "mixed" verticalScrollMode`, () => {
         cy.visit(`http://localhost:3000?${get_query({
-            verticalScrollMode: 'mixed'
+            verticalScrollMode: 'mixed',
+            syntheticScrollAmount: 200
         })}`)
-        cy.get('.button-up').should('be.visible')
+        cy.get('.button-up')
+            .should('be.visible')
+
+        cy.get('.button-down')
+            .should('be.visible')
+            .click()
+
+        cy.get('.matches-positioner').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 0, -200)')
     })
 
     it(`Scroll buttons attain the same width as matches-scroller`, () => {
@@ -30,42 +47,96 @@ describe('Vertical scroll buttons', () => {
         })
     })
 
-    it(`vertical scroll buttons in "gutters" attain a slightly greater height than defaultScrollIconSize`, () => {
+    it(`clicking the inactive scroll button doesn't change synthetic scroll position`, () => {
+        cy.visit(`http://localhost:3000?${get_query({
+            verticalScrollMode: 'buttons'
+        })}`)
+        cy.get('.button-up:not(.active)').click()
+        cy.get('.matches-positioner').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 0, 0)')
+    })
+
+    it(`"gutters" scroll buttons attain a height of defaultScrollIconSize + scrollButtonPadding + borders`, () => {
         cy.visit(`http://localhost:3000?${get_query({
             verticalScrollMode: 'buttons',
             scrollButtonsPosition: 'gutters',
-            defaultScrollIconSize: 29
+            defaultScrollIconSize: 29,
+            scrollButtonPadding: '7px'
         })}`)
-        cy.get('.button-up').should($b => {
-            expect(parseFloat(getComputedStyle($b[0]).height)).to.be.gt(29)
-            expect(parseFloat(getComputedStyle($b[0]).height)).to.be.lt(40)
-        })
-        cy.get('.button-down').should($b => {
-            expect(parseFloat(getComputedStyle($b[0]).height)).to.be.gt(29)
-            expect(parseFloat(getComputedStyle($b[0]).height)).to.be.lt(40)
-        })
+        cy.get('.button-up').should('have.css', 'height', 29 + 14 + 2 + 'px')
+        cy.get('.button-down').should('have.css', 'height', 29 + 14 + 2 + 'px')
     })
 
 
-    it(`vertical scroll buttons in "gutters" attain a height of custom HTML provided by scrollUp/DownButtonHTML`, () => {
+    it(`scrollUp/DownButtonHTML is inserted`, () => {
+        cy.visit(`http://localhost:3000?${get_query({
+            verticalScrollMode: 'buttons',
+            scrollUpButtonHTML: `<div class="up-html">up</div>`,
+            scrollDownButtonHTML: `<div class="down-html">up</div>`,
+        })}`)
+        cy.get('.up-html').should('be.visible')
+        cy.get('.down-html').should('be.visible')
+    })
+
+
+    it(`"gutters" scroll buttons attain a height of scrollUp/DownButtonHTML + scrollButtonPadding + borders`, () => {
         cy.visit(`http://localhost:3000?${get_query({
             verticalScrollMode: 'buttons',
             scrollButtonsPosition: 'gutters',
             scrollUpButtonHTML: `<div style="height: 72px">up</div>`,
-            scrollDownButtonHTML: `<div style="height: 19px">up</div>`
+            scrollDownButtonHTML: `<div style="height: 19px">up</div>`,
+            scrollButtonPadding: '7px'
         })}`)
-        cy.get('.button-up').should('have.css', 'height', '74px') // +2 for borders
-        cy.get('.button-down').should('have.css', 'height', '21px') // +2 for borders
+        cy.get('.button-up').should('have.css', 'height', 72 + 14 + 2 + 'px')
+        cy.get('.button-down').should('have.css', 'height', 19 + 14 + 2 + 'px')
     })
 
 
-    it(`vertical scroll buttons are squashed to 0 height when scrollButtonsPosition is "overMatches"`, () => {
+    it(`scroll buttons are squashed to 0 height when scrollButtonsPosition is "overMatches"`, () => {
         cy.visit(`http://localhost:3000?${get_query({
             verticalScrollMode: 'buttons',
             scrollButtonsPosition: 'overMatches'
         })}`)
         cy.get('.button-up').should('have.css', 'height', '0px')
+        cy.get('.button-down').should('have.css', 'height', '0px')
     })
 
-    // TODO buttons are clickable in all positions
+    it(`scroll icons are visible when "overMatches`, () => {
+        cy.visit(`http://localhost:3000?${get_query({
+            verticalScrollMode: 'buttons',
+            scrollButtonsPosition: 'overMatches'
+        })}`)
+        cy.get('.button-up svg').should('be.visible')
+        cy.get('.button-down svg').should('be.visible')
+    })
+
+    it(`scroll icons are visible when "overMatches`, () => {
+        cy.visit(`http://localhost:3000?${get_query({
+            verticalScrollMode: 'buttons',
+            scrollButtonsPosition: 'overMatches'
+        })}`)
+        cy.get('.button-up svg').should('be.visible')
+        cy.get('.button-down svg').should('be.visible')
+    })
+
+    it(`scroll buttons eat the height of matches if 'gutters'`, () => {
+        cy.visit(`http://localhost:3000?${get_query({
+            verticalScrollMode: 'buttons',
+            scrollButtonsPosition: 'gutters',
+            height: 600,
+            defaultScrollIconSize: 300
+        })}`)
+        cy.get('.matches-scroller').should('have.css', 'height', '0px')
+    })
+
+    it(`scroll buttons do NOT eat the height of matches if 'overMatches'`, () => {
+        cy.visit(`http://localhost:3000?${get_query({
+            verticalScrollMode: 'buttons',
+            scrollButtonsPosition: 'overMatches',
+            height: 600,
+            defaultScrollIconSize: 300
+        })}`)
+        cy.get('.matches-scroller').should($s => {
+            expect($s[0].clientHeight).to.be.gt(400)
+        })
+    })
 })
