e0e82c0a3ad24e1254ed7f91cd1ce4b8657b8712
Sergey Bachinin <sbachinin@yandex.ru>
2023-03-20 13:01:15 +0530

    test size

--- cypress/e2e/size.cy.js
+++ cypress/e2e/size.cy.js
@@ -3,7 +3,7 @@ import { get_query } from './get_query.js'
 describe(`Bracket's size`, () => {
 
     beforeEach(() => cy.viewport(1280, 720))
-    
+
     it(`stretches to full width of a wrapper when "width" option is not specified`, () => {
         cy.visit(`http://localhost:3000/cypress`)
 
@@ -23,22 +23,52 @@ describe(`Bracket's size`, () => {
             .should('have.css', 'height', '600px')
     })
 
-    it(`becomes wider than user_wrapper when 'width' option is set to a greater number`, () => {
-        
+    it(`does not become wider than user_wrapper even if 'width' option is set to a greater number`, () => {
+
         cy.visit(`http://localhost:3000/cypress?${get_query({
             width: '2000px'
         })}`)
 
         cy.get('.bracket-root')
-            .should('have.css', 'width', '2000px')
+            .should('have.css', 'width', '1280px')
             .and($s => {
-                expect(getComputedStyle($s[0]).width).to.equal('2000px')
+                expect(getComputedStyle($s[0]).width).to.equal('1280px')
+            })
+    })
+
+
+    it(`bracket-root is narrower than user_wrapper if actual content is narrower`, () => {
+
+        cy.viewport(3000, 720)
+
+        cy.visit(`http://localhost:3000/cypress`)
+
+        cy.get('.bracket-root')
+            .should('have.css', 'width')
+            .and(width => {
+                expect(parseInt(width)).to.be.lt(2500)
+            })
+    })
+
+
+    it(`bracket-root's width is 100% of the parent when actual content is narrower but width option is set to 100%`, () => {
+
+        cy.viewport(3000, 720)
+
+        cy.visit(`http://localhost:3000/cypress?${get_query({
+            width: '100%'
+        })}`)
+
+        cy.get('.bracket-root')
+            .should('have.css', 'width', '3000px')
+            .and($s => {
+                expect(getComputedStyle($s[0]).width).to.equal('3000px')
             })
     })
 
 
     it(`becomes taller than user_wrapper when 'height' option is set to a greater number`, () => {
-        
+
         cy.visit(`http://localhost:3000/cypress?${get_query({
             height: '2000px'
         })}`)
